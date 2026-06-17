854be684c742bddbeff3dd0c04f7fd44baa1edba
EVOZXLaunch <evozxtoken@gmail.com>
2026-06-17 21:44:57 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,23 +1,26 @@
 export const CONFIG = {
 
-CHAIN_ID: 805,
+    RPC:
+        "https://rpc.evozscan.com",
 
-RPC_URL:
-"https://rpc.evozscan.com",
+    CHAIN_ID:
+        805,
 
-EXPLORER:
-"https://evozscan.com",
+    EXPLORER:
+        "https://evozscan.com",
 
-FACTORY:
-"0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
+    EVOZX:
+        "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
 
-EVOZX:
-"0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
+    FACTORY:
+        "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
 
-EXCHANGE:
-"0x24cCb720F7F8b9247FB50A88F6A6a5A5DD7d9ab8",
+    EXCHANGE:
+        "0x24cCb720F7F8b9247FB50A88F6A6a5A5DD7d9ab8",
 
-TREASURY:
-"0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
+    TREASURY:
+        "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C",
 
+    VERIFICATION_PACKAGE:
+        "./verification/standard-input.json"
 };
